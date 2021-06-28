8062c4e0cf862fcbf61d2c03b07b62e73a542ac6
Kaveesha Ranmuthu <kranmuthu1999@gmail.com>
2021-06-29 11:41:31 +1200

    Expenses completed

--- solution.py
+++ solution.py
@@ -6,14 +6,20 @@ with open("data.json", "r") as inp:
 all_data = json.loads(data_read)
 data = all_data["data"]
 
-# -- REVENUE --
+# -- REVENUE AND EXPENSES --
 revenue = 0
+expenses = 0
 for d in data:
     account_cat = d["account_category"]
-    if (account_cat == "revenue"):
+    if account_cat == "revenue":
         total_amt = d["total_value"]
         revenue += total_amt
+    elif account_cat == "expense":
+        total_amt = d["total_value"]
+        expenses += total_amt
 
-revenue = int(round(revenue))
+revenue_rounded = int(round(revenue))
+expenses_rounded = int(round(expenses))
 
-print("Revenue: ${:,}".format(revenue))
+print("Revenue: ${:,}".format(revenue_rounded))
+print("Expenses: ${:,}".format(expenses_rounded))
